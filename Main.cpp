f1604c4a6b378a98dc3b7063ef71f729aba2ded6
samueltenka <iguavadon@gmail.com>
2015-01-25 04:37:54 -0500

    Update Main.cpp
    still works.

--- Main.cpp
+++ Main.cpp
@@ -1,54 +1,20 @@
-#include <stdio.h>
+#include <math.h>
+#include "VectorCSV.h"
 
 
-struct Vector {
-   int dim;
-   float* sub;
-   Vector(int dim): dim(dim) {
-      sub = new float[dim];
-   } ~Vector() {
-      delete[] sub;
-   }
-};
-struct VectorList {
-   int num;
-   Vector** vecs;
-   VectorList(int num, int dim): num(num) {
-      vecs = (new Vector*[num]);
-      for(int n=0; n<num; n++) { vecs[n] = new Vector(dim); }
-   } ~VectorList() {
-      for(int n=0; n<num; n++) { delete vecs[n]; }
-      delete[] vecs;
-   }
-   float* get(int n, int d) { return &(vecs[n]->sub[d]); }
-};
 
-
-const int N = 784;//7830;//50000;
+const int N = 784;//50000;
 const int DIM = 28*28;
+VectorList xs(N, DIM);
+int ts[N];
 
 void main() {
    /************************
     READ TRAINING DATA
     ************************/
-   VectorList xs(N, DIM);
-   int ts[N];
+   init_from("train_usps_shorter.csv", N, DIM, xs, ts);
 
-   FILE* train_file;
-   fopen_s(&train_file, "train_usps_shorter.csv", "r");
-   char line[10000];
-   fgets(line, 10000, train_file); // read heading
-   for(int n=0; n<N; n++) { // read body
-      int DEE;
-      fscanf_s(train_file, "%10f,", &DEE); // (id)
-      for(int d=0; d<DIM; d++) {
-         fscanf_s(train_file, "%f,", xs.get(n, d));
-      } // (x's)
-      fscanf_s(train_file, "%1d", &ts[n]);
-      if(n%1000==0) {printf("!%d\n", n);}
-   }
-   printf("%d %f %f\n", ts[0], *xs.get(0, 0), *xs.get(N-1, 157));
-   fclose(train_file);
+
 
    printf("I have survived!\n");
    char c; scanf_s("%c", &c);
