6755a601ce88dbf242417604d326dba2470d436c
samueltenka <iguavadon@gmail.com>
2015-01-25 05:21:49 -0500

    Update Main.cpp
    Model error calculator...

--- Main.cpp
+++ Main.cpp
@@ -4,8 +4,8 @@
 #include "VectorCSV.h"
 
 
-const float lambda = 0.00001;
-const float timestep = 0.0001;
+const float reg_param = 0.01;
+const float timestep = 0.01;
 const int REPS = 200;
 
 const int K = 10;
@@ -24,8 +24,29 @@ float prob(int t, Vector &x) {
 }
 void step(int k, float dt) {
    Vector grad(DIM);
+   grad.zero_out();
+   grad += weights.vecs[k]->times(reg_param);
    for(int n=0; n<N; n++) { grad += xs.vecs[n]->times(coeff(k, n)); }
    *weights.vecs[k] += grad.times(-dt);
+}
+
+int classify(Vector& x) { // t of maximum probability P(t|x,w's) given predetermined w's.
+   int best_k=0;
+   float best_prob = prob(0, x);
+   for(int k=1; k<K; k++) {
+      float new_prob = prob(k, x);
+      if(new_prob > best_prob) {
+         best_k = k;
+         best_prob = new_prob;
+      }
+   }
+   return best_k;
+} int error_of(Vector& x, int correct_t) {
+   return (classify(x)==correct_t ? 0 : 1);
+} float error_on(VectorList &test_xs, int* const &correct_ts) {
+   float sum = 0.0;
+   for(int n=0; n<test_xs.num; n++) { sum += error_of(*(test_xs.vecs[n]), correct_ts[n]); }
+   return sum/test_xs.num;
 }
 
 void main() {
@@ -43,7 +64,7 @@ void main() {
    weights.zero_out();
    for(int i=0; i<REPS; i++) {
       step(rand()%K, timestep);
-      if(i%10==0) {printf("%d\n", i);}
+      if(i%10==0) {printf("%d %f\n", i, error_on(xs, ts));}
    }
    printf("UPDATING DONE!\n");
 
