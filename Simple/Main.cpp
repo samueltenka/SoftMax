e86d46471410ec29b754db34cc8be548cafda7f2
samueltenka <iguavadon@gmail.com>
2015-02-08 13:28:48 -0500

    Update Main.cpp
    experimenting w/ fourier...

--- Simple/Main.cpp
+++ Simple/Main.cpp
@@ -2,46 +2,63 @@
 //
 
 #include "CSV.h"
+#include "SamVectorize.h"
 #include "Fourier.h"
 #include "Randomize.h"
 #include "SoftMax.h"
 #include "Parameters.h"
+#include "Poly.h"
 
-const double reg_param = 0.000001;
-const double dt = 0.00001;
+const double reg_param = 0.0000001;
+const double dt = 0.000001;
 const int REPS = 200;
 
-float** xs;//[LEN][DIM]
+float** xs;//[LEN][XDIM]
 int ts[LEN];
-float** ps;//[LEN][PDIM]
+float** ps;//[LEN][MDIM]
+float** qs;//[LEN][QDIM]
 float** ws;//[K][DIM]
 
 
 int main() {
    // SET UP
    //
-   construct(xs, LEN, DIM);
-   construct(ps, LEN, PDIM);
+   construct(xs, LEN, XDIM);
+   construct(ps, LEN, MDIM);
+   //construct(qs, LEN, QDIM);
    construct(ws, K, DIM);
 
-   //seed_random();
+   seed_random();
    make_waves();
-   read_xts_from("train_usps_short.csv", xs, ts, LEN, DIM);
-   for(int n=0; n<LEN; n++) {fourier_analyze(xs[n], ps[n]);}
-   write_xs_to("fouriers.csv", ps, LEN, PDIM);
+   read_xts_from("train_usps_short.csv", xs, ts, LEN, XDIM);
+   for(int n=0; n<LEN; n++) {
+      fourier_analyze(xs[n], ps[n]);
+      polynomialize(ps[n], qs[n]);
+   }
+   //write_xs_to("features.csv", qs, LEN, QDIM);
 
    // SIMULATE
    //
+   float wtemp[DIM]; double last_error=1.0;
    for(int count=0; count<REPS; ++count) {
-      step(random()%K, dt, reg_param, ts, ps, ws);
+      int k = random()%K;
+      times(wtemp, ws[k], 1.0, DIM);
+
+      step(k, dt, reg_param, ts, ps, ws);
+
       double e = error_on(ts, ps, ws, LEN);
-      printf("%d \t %f\n", count, e);
+      if(e>last_error && random()%10!=0) {times(ws[k], wtemp, 1.0, DIM);}
+      else {last_error = e;}
+      
+      printf("%d \t %f \t %f\n", count, last_error, e);
    }
+   write_xs_to("weights_.csv", ws, K, DIM);
 
    // CLEAN UP
    //
    destroy(xs, LEN);
    destroy(ps, LEN);
+   //destroy(qs, LEN);
    destroy(ws, K);
    return 0;
 }
