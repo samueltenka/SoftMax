538198c2c4991da6ec82b69cdc66c9f00d6332fb
samueltenka <iguavadon@gmail.com>
2015-02-08 04:25:31 -0500

    Update Main.cpp
    Runs without choking!

--- Simple/Main.cpp
+++ Simple/Main.cpp
@@ -1,21 +1,15 @@
 // Creation 2015 by Samuel Tenka
 //
 
-#include <stdlib.h>  /* srand, rand*/
-#include <time.h>    /* time */
-
 #include "CSV.h"
-#include "SamVectorize.h"
 #include "Fourier.h"
+#include "Randomize.h"
+#include "SoftMax.h"
+#include "Parameters.h"
 
 const double reg_param = 0.000001;
 const double dt = 0.000001;
 const int REPS = 200;
-
-const int LEN = 7840;
-const int K = 10;
-//const int DIM = 28*28;
-const int PDIM = 3*3;
 
 float** xs;//[LEN][DIM]
 int ts[LEN];
@@ -24,18 +18,27 @@ float** ws;//[K][DIM]
 
 
 int main() {
+   // SET UP
+   //
    construct(xs, LEN, DIM);
    construct(ps, LEN, PDIM);
    construct(ws, K, DIM);
 
+   //seed_random();
    make_waves();
    read_xts_from("train_usps_short.csv", xs, ts, LEN, DIM);
-   for(int n=0; n<LEN; n++) {
-      if(n%100==0) {printf("%d\n", n);}
-      fourier_analyze(xs[n], ps[n]);
-   }
+   for(int n=0; n<LEN; n++) {fourier_analyze(xs[n], ps[n]);}
    write_xs_to("fouriers.csv", ps, LEN, PDIM);
 
+   // SIMULATE
+   //
+   for(int count=0; count<REPS; ++count) {
+      step(random()%K, dt, reg_param, ts, ps, ws);
+      printf("hi!\n");
+   }
+
+   // CLEAN UP
+   //
    destroy(xs, LEN);
    destroy(ps, LEN);
    destroy(ws, K);
