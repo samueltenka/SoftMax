61340b42b31b5081f1f31d1bbb07c19633ce07c8
samueltenka <iguavadon@gmail.com>
2015-02-08 03:56:16 -0500

    Update Main.cpp
    Fourier analysis works!

--- Simple/Main.cpp
+++ Simple/Main.cpp
@@ -12,27 +12,32 @@ const double reg_param = 0.000001;
 const double dt = 0.000001;
 const int REPS = 200;
 
-const int LEN = 7840; //50000;
+const int LEN = 7840;
 const int K = 10;
-const int DIM = 28*28;
+//const int DIM = 28*28;
+const int PDIM = 3*3;
 
 float** xs;//[LEN][DIM]
 int ts[LEN];
+float** ps;//[LEN][PDIM]
 float** ws;//[K][DIM]
 
 
 int main() {
-   //construct(xs, LEN, DIM);
+   construct(xs, LEN, DIM);
+   construct(ps, LEN, PDIM);
    construct(ws, K, DIM);
 
-   for(int k=0; k<K; ++k) {
-      initialize_fourier(k, k, ws[k]);
+   make_waves();
+   read_xts_from("train_usps_short.csv", xs, ts, LEN, DIM);
+   for(int n=0; n<LEN; n++) {
+      if(n%100==0) {printf("%d\n", n);}
+      fourier_analyze(xs[n], ps[n]);
    }
+   write_xs_to("fouriers.csv", ps, LEN, PDIM);
 
-   //read_xs_from("weights_1.csv", ws, K, DIM);
-   write_xs_to("fouriers.csv", ws, K, DIM);
-
-   //destroy(xs, LEN);
+   destroy(xs, LEN);
+   destroy(ps, LEN);
    destroy(ws, K);
    return 0;
 }
