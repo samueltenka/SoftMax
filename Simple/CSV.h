90689df33802aa78057d6bc534820ce110aaac10
samueltenka <iguavadon@gmail.com>
2015-02-08 03:08:43 -0500

    Update CSV.h
    Migration to pure arrays is complete!

--- Simple/CSV.h
+++ Simple/CSV.h
@@ -6,37 +6,60 @@
 #define STDIO_H
 #endif
 
-#ifndef VECTOR_H
-#include "SamVector.h"
-#define VECTOR_H
-#endif
 
-void read_xts_from(const char* filename, int DIM, VectorList &xs, int* const &ts) {
-   FILE* train_file = fopen(filename, "r");
+void read_xts_from(const char* filename, float** &xs, int* &ts, const int len, const int dim) {
+   FILE* train_file;
+   fopen_s(&train_file, filename, "r");
    char line[10000];
    fgets(line, 10000, train_file); // read heading
-   for(int n=0; n<xs.num; n++) { // read body
-      fscanf(train_file, "%*10f,"); // (id)
-      for(int d=0; d<DIM; d++) {fscanf(train_file, "%f,", xs.get(n, d));} // (x's)
-      fscanf(train_file, "%1d", &ts[n]);  // (t's)
+   for(int n=0; n<len; ++n) { // read body
+      fscanf_s(train_file, "%*10f,"); // (id)
+      for(int i=0; i<dim; ++i) {fscanf_s(train_file, "%f,", &xs[n][i]);} // (x's)
+      fscanf_s(train_file, "%1d", &ts[n]);  // (t's)
 
       if(n%1000==0) {printf("reading xt's: n=%d\n", n);}
    }
    fclose(train_file);
+} void read_xs_from(const char* filename, float** &xs, const int len, const int dim) {
+   FILE* train_file;
+   fopen_s(&train_file, filename, "r");
+   char line[10000];
+   fgets(line, 10000, train_file); // read heading
+   for(int n=0; n<len; ++n) { // read body
+      fscanf_s(train_file, "%*10f,"); // (id)
+      int i; for(i=0; i<dim-1; ++i) {fscanf_s(train_file, "%f,", &xs[n][i]);} // (x's)
+      fscanf_s(train_file, "%f", &xs[n][i]); // (last x of row)
+
+      if(n%1000==0) {printf("reading x's: n=%d\n", n);}
+   }
+   fclose(train_file);
 }
 
-void write_ws_to(const char* filename, int DIM, VectorList &ws) {
-   FILE* weight_file = fopen(filename, "w");
-   fprintf(weight_file, "id,"); // write heading
-   for(int d=0; d<DIM-1; d++) {
-      fprintf(weight_file, "%d,", d);
-   } fprintf(weight_file, "%d\n", DIM-1);
+void write_ts_to(const char* filename, int* &ts, const int len) {
+   FILE* predict_file;
+   fopen_s(&predict_file, filename, "w");
 
-   for(int n=0; n<ws.num; n++) { // write body
+   fprintf(predict_file, "id,label\n"); // write heading
+   for(int n=0; n<len; ++n) { // write t's
+      fprintf(predict_file, "%d,%d\n", n, ts[n]);
+   }
+   fclose(predict_file);
+}
+
+void write_xs_to(const char* filename, float** &xs, const int len, const int dim) {
+   FILE* weight_file;
+   fopen_s(&weight_file, filename, "w");
+
+   fprintf(weight_file, "id,"); // write heading
+   int i; for(i=0; i<dim-1; ++i) {
+      fprintf(weight_file, "%d,", i);
+   } fprintf(weight_file, "%d\n", i);
+
+   for(int n=0; n<len; n++) { // write body
       fprintf(weight_file, "%d,", n);
-      for(int d=0; d<DIM-1; d++) {
-         fprintf(weight_file, "%f,", *ws.get(n,d));
-      } fprintf(weight_file, "%f\n", *ws.get(n,DIM-1));
+      int i; for(i=0; i<dim-1; ++i) {
+         fprintf(weight_file, "%f,", xs[n][i]);
+      } fprintf(weight_file, "%f\n", xs[n][i]);
    }
    fclose(weight_file);
 }
