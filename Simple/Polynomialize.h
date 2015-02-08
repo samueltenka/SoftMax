5ba0d4df36a5a1660882ef8ca94883dd2b9efe38
samueltenka <iguavadon@gmail.com>
2015-02-08 04:35:57 -0500

    Create Polynomialize.h
    for turning momentum features into quadratic features

---
+++ Simple/Polynomialize.h
@@ -0,0 +1,11 @@
+#include "Parameters.h"
+
+void polynomialize(const float p[], float poly[]) {
+   int i=0;
+   for(int j=0; j<PDIM; ++j) {
+      poly[i++] = p[j];
+      for(int k=j; k<PSIDE; ++k) {
+         poly[i++] = p[j]*p[k]/2;
+      }
+   }
+}
