2a58d93930ebed66f8dabd51a90a7b78e316ce92
samueltenka <iguavadon@gmail.com>
2015-02-08 13:43:14 -0500

    Update Main.cpp
    double-application is NOT as good as single.

--- Simple/Main.cpp
+++ Simple/Main.cpp
@@ -2,21 +2,24 @@
 //
 
 #include "CSV.h"
+#include "Parameters.h"
 #include "SamVectorize.h"
-#include "Fourier.h"
+//#include "Fourier.h"
+//#include "Poly.h"
+#include "Edge.h"
 #include "Randomize.h"
 #include "SoftMax.h"
-#include "Parameters.h"
-#include "Poly.h"
 
-const double reg_param = 0.0000001;
-const double dt = 0.000001;
+const double reg_param = 0.00001;
+const double dt = 0.0001;
 const int REPS = 200;
 
 float** xs;//[LEN][XDIM]
 int ts[LEN];
-float** ps;//[LEN][MDIM]
-float** qs;//[LEN][QDIM]
+float** z1s;//[LEN][XDIM]
+float** z2s;//[LEN][XDIM]
+//float** ps;//[LEN][MDIM]
+//float** qs;//[LEN][QDIM]
 float** ws;//[K][DIM]
 
 
@@ -24,18 +27,21 @@ int main() {
    // SET UP
    //
    construct(xs, LEN, XDIM);
-   construct(ps, LEN, MDIM);
+   construct(z1s, LEN, XDIM);
+   construct(z2s, LEN, XDIM);
+   //construct(ps, LEN, MDIM);
    //construct(qs, LEN, QDIM);
    construct(ws, K, DIM);
 
    seed_random();
-   make_waves();
    read_xts_from("train_usps_short.csv", xs, ts, LEN, XDIM);
+   //make_waves();
    for(int n=0; n<LEN; n++) {
-      fourier_analyze(xs[n], ps[n]);
-      polynomialize(ps[n], qs[n]);
+      edge_detect(xs[n], z2s[n]);
+   //   fourier_analyze(xs[n], ps[n]);
+   //   polynomialize(ps[n], qs[n]);
    }
-   //write_xs_to("features.csv", qs, LEN, QDIM);
+   write_xs_to("features_edges.csv", z2s, LEN, DIM);
 
    // SIMULATE
    //
@@ -44,9 +50,9 @@ int main() {
       int k = random()%K;
       times(wtemp, ws[k], 1.0, DIM);
 
-      step(k, dt, reg_param, ts, ps, ws);
+      step(k, dt, reg_param, ts, z2s, ws);
 
-      double e = error_on(ts, ps, ws, LEN);
+      double e = error_on(ts, z2s, ws, LEN);
       if(e>last_error && random()%10!=0) {times(ws[k], wtemp, 1.0, DIM);}
       else {last_error = e;}
       
@@ -57,7 +63,9 @@ int main() {
    // CLEAN UP
    //
    destroy(xs, LEN);
-   destroy(ps, LEN);
+   destroy(z1s, LEN);
+   destroy(z2s, LEN);
+   //destroy(ps, LEN);
    //destroy(qs, LEN);
    destroy(ws, K);
    return 0;
