0def828d3c3aaf831b8d3ea1a0de72b0448c1e83
samueltenka <iguavadon@gmail.com>
2015-02-08 13:50:56 -0500

    Update Main.cpp
    fixed 2*(c+j) parentheses bug (now 2*c+j).
    Also extended edges to be length 3.

--- Simple/Main.cpp
+++ Simple/Main.cpp
@@ -1,72 +1,51 @@
-// Creation 2015 by Samuel Tenka
-//
-
-#include "CSV.h"
 #include "Parameters.h"
 #include "SamVectorize.h"
-//#include "Fourier.h"
-//#include "Poly.h"
-#include "Edge.h"
-#include "Randomize.h"
-#include "SoftMax.h"
-
-const double reg_param = 0.00001;
-const double dt = 0.0001;
-const int REPS = 200;
-
-float** xs;//[LEN][XDIM]
-int ts[LEN];
-float** z1s;//[LEN][XDIM]
-float** z2s;//[LEN][XDIM]
-//float** ps;//[LEN][MDIM]
-//float** qs;//[LEN][QDIM]
-float** ws;//[K][DIM]
 
 
-int main() {
-   // SET UP
-   //
-   construct(xs, LEN, XDIM);
-   construct(z1s, LEN, XDIM);
-   construct(z2s, LEN, XDIM);
-   //construct(ps, LEN, MDIM);
-   //construct(qs, LEN, QDIM);
-   construct(ws, K, DIM);
+static void shrink(const float x[], float s[]) {
+   zero_out(s, HDIM);
+   for(int r=0; r<HALF; ++r) {
+      for(int c=0; c<HALF; ++c) {
 
-   seed_random();
-   read_xts_from("train_usps_short.csv", xs, ts, LEN, XDIM);
-   //make_waves();
-   for(int n=0; n<LEN; n++) {
-      edge_detect(xs[n], z2s[n]);
-   //   fourier_analyze(xs[n], ps[n]);
-   //   polynomialize(ps[n], qs[n]);
+         // avg:
+         for(int i=0; i<2; ++i) {
+            for(int j=0; j<2; ++j) {
+            s[r*HALF+c] += x[(2*r+i)*SIDE+2*c+j];
+            }
+         }
+         s[r*HALF+c] /= 4;
+
+      }
    }
-   write_xs_to("features_edges.csv", z2s, LEN, DIM);
+}
 
-   // SIMULATE
-   //
-   float wtemp[DIM]; double last_error=1.0;
-   for(int count=0; count<REPS; ++count) {
-      int k = random()%K;
-      times(wtemp, ws[k], 1.0, DIM);
+static float edginess(float a, float b, float c) { // use harmonic mean as rounded minimum
+   return (a+b+c!=0) ? 3*a*b/(a+b+c) : 0;
+}
+static float get_edge(int i, int j, int r, int c, const float x[]) {
+   if(i!=0 || j!= 0) {
+      return edginess(x[(2*r-i)*SIDE + 2*c-j],
+                      x[(2*r)*SIDE + 2*c],
+                      x[(2*r+i)*SIDE + 2*c+j]);
+   } return edginess(x[(2*r+1)*SIDE + 2*c-1],
+                     x[(2*r)*SIDE + 2*c],
+                     x[(2*r-1)*SIDE + 2*c+1]);
+}
 
-      step(k, dt, reg_param, ts, z2s, ws);
+void edge_detect(const float x[], float z[]) {
+   float small[HDIM]; shrink(x, small);
 
-      double e = error_on(ts, z2s, ws, LEN);
-      if(e>last_error && random()%10!=0) {times(ws[k], wtemp, 1.0, DIM);}
-      else {last_error = e;}
-      
-      printf("%d \t %f \t %f\n", count, last_error, e);
+   zero_out(z, XDIM); // in particular, there will be a black window separating 4 regions
+   for(int i=0; i<2; ++i) {
+      for(int j=0; j<2; ++j) {
+
+         // edge:
+         for(int r=1; r<HALF-1; ++r) {
+            for(int c=1; c<HALF-1; ++c) {
+               z[(i*HALF+r)*SIDE + j*HALF+c] = get_edge(i,j,r,c, x);
+            }
+         }
+
+      }
    }
-   write_xs_to("weights_.csv", ws, K, DIM);
-
-   // CLEAN UP
-   //
-   destroy(xs, LEN);
-   destroy(z1s, LEN);
-   destroy(z2s, LEN);
-   //destroy(ps, LEN);
-   //destroy(qs, LEN);
-   destroy(ws, K);
-   return 0;
 }
