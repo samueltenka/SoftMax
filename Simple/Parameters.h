6ccc957ea37b74fb583c154023f02a1c592be29f
samueltenka <iguavadon@gmail.com>
2015-02-08 04:24:42 -0500

    Create Parameters.h

---
+++ Simple/Parameters.h
@@ -0,0 +1,14 @@
+#ifndef PARAMETERS_H
+#define PARAMETERS_H
+
+const int K = 10;
+const int LEN = 7840;
+
+const int SIDE = 28;
+const int DIM = SIDE*SIDE;
+const int PSIDE = 3;
+const int PDIM = PSIDE*PSIDE;
+
+const double PI = 3.14159265358979323;
+
+#endif
